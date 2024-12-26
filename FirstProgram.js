c787e9843f0ca8560492dec5afda4d67fd296fbf
THODETI NARENDER <tnr123457@gmail.com>
2024-12-26 21:18:34 +0530

    getElementsByClassName

--- FirstProgram.js
+++ FirstProgram.js
@@ -1,3 +1,5 @@
+//getElementById
+
 const mainHeading = document.getElementById('main-heading');
 
 mainHeading.textContent = 'Fruit World';
@@ -12,3 +14,13 @@ basketHeading.style.color = 'green';
 
 const thanks = document.getElementById('thanks');
 thanks.innerHTML = '<p>Please visit us again</p>';
+
+//getElementsByClassName
+
+const fruit = document.getElementsByClassName('fruit');
+
+fruit[2].style.backgroundColor = 'yellow';
+
+for (let i = 0; i < fruit.length;i++) {
+    fruit[i].style.fontWeight = 'bold';
+}
