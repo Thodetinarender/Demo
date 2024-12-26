def18d32c0daa2d58137a8170c0e57d9ea27a178
THODETI NARENDER <tnr123457@gmail.com>
2024-12-26 21:32:01 +0530

    getElementsByTagName

--- FirstProgram.js
+++ FirstProgram.js
@@ -11,7 +11,7 @@ header.style.borderBottom = '2px solid orange';
 
 const basketHeading = document.getElementById('basket-heading');
 basketHeading.style.color = 'green';
-
+ 
 const thanks = document.getElementById('thanks');
 thanks.innerHTML = '<p>Please visit us again</p>';
 
@@ -23,4 +23,14 @@ fruit[2].style.backgroundColor = 'yellow';
 
 for (let i = 0; i < fruit.length;i++) {
     fruit[i].style.fontWeight = 'bold';
+}
+
+//getElementsByTagName
+
+const line = document.getElementsByTagName('li');
+
+line[4].style.color = 'blue';
+
+for (let i = 0; i < line.length; i++){
+    line[i].style.fontStyle = 'italic';
 }
